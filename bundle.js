e9f359bf094b9e22649e7175e035f444dbcb7a33
Paul Serraino <pserraino99@gmail.com>
2015-01-11 18:59:14 -0600

    streaming video data from canvas typed array

--- bundle.js
+++ bundle.js
@@ -1,9 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Engine = require('engine.io-stream');
-var con = Engine('/engine');
+var socket = Engine('/engine');
 var hat = require('hat');
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", main, false);
+
+function main() {
 
 	var hash = window.location.hash;
 	if (hash === "" || !hash) {
@@ -12,30 +14,57 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	var msg = document.getElementById('msg'),
 		msgList = document.getElementById('msgList'),
-		video = document.getElementById('video');
+		video = document.getElementById('video'),
+		videoList = document.getElementById('videoList'),
+		canvas = document.getElementById('capture');
 		
 
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia; 
-	navigator.getUserMedia({video: true, audio: true}, function (videoStream) {
+	navigator.getUserMedia({video: true, audio: false/*true*/}, function (videoStream) {
 		video.src = window.URL.createObjectURL(videoStream);
+
 	}, function failed(){});
+
+
+	video.addEventListener('timeupdate', function (e) {
+		//hack: convert each frame to image & stream images
+		var ctx = canvas.getContext('2d');
+		ctx.drawImage(video, 0, 0);
+		
+		//Uint8ClampedArray data type
+		socket.write(JSON.stringify({
+			type: "video",
+			data: ctx.getImageData(0, 0, 320, 240)
+		}));
+
+	}, false);
+
 
 	msg.addEventListener("keyup", function (evt) {
 		if (evt.keyCode === 13 && evt.target.value) {
-			con.write(JSON.stringify({
+			socket.write(JSON.stringify({
+				type: "text",
 				id: hash,
 				message: evt.target.value
 			}));
 			evt.target.value = null;
 		}
-	}, false)
+	}, false);
 
-	con.on('data', function (data) {
+	socket.on('data', function (data) {
+		var d = data;
 		var li = document.createElement('li');
-		li.appendChild(document.createTextNode(data));
-		msgList.appendChild(li);
-	})
-});
+
+		if (typeof data === "string") d = JSON.parse(d);
+		if (d.type === "text") {
+			li.appendChild(document.createTextNode(d.message));
+			msgList.appendChild(li);
+
+		} else if (d.type === "video") {
+			console.log('video data ', d)
+		}
+	});
+}
 
 },{"engine.io-stream":23,"hat":47}],2:[function(require,module,exports){
 /*!
